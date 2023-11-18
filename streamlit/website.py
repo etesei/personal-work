b57febd58f38aca7918ded77dbda07c284ef6955
Evan <ejtesei@gmail.com>
2023-11-17 22:25:55 -0500

    upd

--- streamlit/website.py
+++ streamlit/website.py
@@ -1,7 +1,28 @@
 import streamlit as st
-import altair as alt
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Title of the web app
 st.title('Rustic Italian Restaurant Dashboard')
 
-# A simple textt
+data = pd.read_csv('data/output.csv')
+
+# Display the data on the website
+st.write(data)
+
+# Create a bar chart from the data
+chart_data = data[data['category']=='total_net_sales'].groupby('date').value.sum().reset_index()
+
+print(chart_data)
+fig, ax = plt.subplots()
+chart_data.plot(kind='bar', ax=ax)
+ax.set_xlabel('date')
+ax.set_ylabel('sales')
+
+st.pyplot(fig)
+
+
+
+
+
+# # A simple textt
